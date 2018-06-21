cf0abc3ae07c5c300fb119dd997b2ee949bbfb02
Patrick Recher <p@tte.io>
2018-06-21 17:38:26 +0200

    feat(InlineFragment): add type of InlineFragment if true is supplied as second argument

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function dotConcat(a, b) {
     return a ? `${a}.${b}` : b;
 }
 
-function getFieldSet(info, asts = info.fieldASTs || info.fieldNodes, prefix = '') {
+function getFieldSet(info, asts = info.fieldASTs || info.fieldNodes, prefix = '', namedInlineFragments = false) {
     // for recursion: fragments doesn't have many sets
     if (!Array.isArray(asts)) {
         asts = [asts];
@@ -49,19 +49,22 @@ function getFieldSet(info, asts = info.fieldASTs || info.fieldNodes, prefix = ''
             case 'Field':
                 const newPrefix = dotConcat(prefix, ast.name.value);
                 if (ast.selectionSet) {
-                    return Object.assign({}, set, getFieldSet(info, ast, newPrefix));
+                    return Object.assign({}, set, getFieldSet(info, ast, newPrefix, namedInlineFragments));
                 } else {
                     set[newPrefix] = true;
                     return set;
                 }
             case 'InlineFragment':
-                return Object.assign({}, set, getFieldSet(info, ast, prefix));
+                const nextPrefix = namedInlineFragments
+                  ? dotConcat(prefix, ast.typeCondition.name.value)
+                  : prefix
+                return Object.assign({}, set, getFieldSet(info, ast, nextPrefix, namedInlineFragments));
             case 'FragmentSpread':
-                return Object.assign({}, set, getFieldSet(info, info.fragments[ast.name.value], prefix));
+                return Object.assign({}, set, getFieldSet(info, info.fragments[ast.name.value], prefix, namedInlineFragments));
         }
     }, {});
 }
 
-module.exports = function getFieldList(info) {
-    return Object.keys(getFieldSet(info));
+module.exports = function getFieldList(info, namedInlineFragments) {
+    return Object.keys(getFieldSet(info, undefined, undefined, namedInlineFragments));
 };
